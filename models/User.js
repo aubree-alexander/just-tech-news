5d75d6a7d635a2b0b9afdc8906e935e0d99d4aa9
Aubree Alexander <aubree.alexander@gmail.com>
2022-05-16 20:57:14 -0500

    add security and authentication

--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 //create User model
-class User extends Model {}
+class User extends Model {
+    //set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 //define table columns and configuration 
 User.init(
@@ -44,7 +50,20 @@ User.init(
             }
         }
     },
+    
     {
+        hooks: {
+            //set up beforeCreate lifecycle 'hook' functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            //set up beforeUpdate lifecycle 'hook' functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+    },
         //table configuration options go here
 
         //pass in imported sequelize connection (direct connection to our database)
@@ -59,6 +78,10 @@ User.init(
         modelName: 'user'
     }
 );
+
+bcrypt.hash(myPlaintextPassword, saltRounds).then(function(hash) {
+    //store hash in your password DB
+});
 
 
 module.exports = User;
