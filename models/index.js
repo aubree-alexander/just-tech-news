d4ef30d8ab3f14f7281ced9e76bd6c3c9ea77675
Aubree Alexander <aubree.alexander@gmail.com>
2022-05-21 21:11:38 -0500

    add post-routes.js file

--- models/index.js
+++ models/index.js
@@ -1,4 +1,16 @@
 const User = require('./User');
 const Post = require('./Post');
 
+
+
+//create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+//post can only belong to 1 user
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Post };
