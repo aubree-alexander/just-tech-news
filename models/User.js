7d36cfc1de8677098e14604ec20f60ba86760c00
Aubree Alexander <aubree.alexander@gmail.com>
2022-05-22 12:45:44 -0500

    allow user to vote on posts

--- models/User.js
+++ models/User.js
@@ -1,87 +1,63 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-//create User model
+// create our User model
 class User extends Model {
-    //set up method to run on instance data (per user) to check password
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
 }
 
-//define table columns and configuration 
+// create fields/columns for User model
 User.init(
-    {
-        //define an id column
-        id: {
-            //user special Sequelize DataTypes object to provide what type of data it is
-            type: DataTypes.INTEGER,
-            //equivalent of sql's 'not null' option
-            allowNull: false,
-            //instruct that this is primary key
-            primaryKey: true,
-            //turn on auto increment
-            autoIncrement: true
-        },
-        //define username column
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        //define email column
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            //cannot be duplicate email values in the table
-            unique: true,
-            //if allownull is set to false, we can run our data through validators before creating the table data
-            validate: {
-                isEmail: true
-            }
-        },
-        //define password column
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                //this means password must be at least 4 characters long
-                len: [4]
-            }
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
-    
-    {
-        hooks: {
-            //set up beforeCreate lifecycle 'hook' functionality
-            async beforeCreate(newUserData) {
-                newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                return newUserData;
-            },
-            //set up beforeUpdate lifecycle 'hook' functionality
-            async beforeUpdate(updatedUserData) {
-                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-                return updatedUserData;
-            }
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-        //table configuration options go here
-
-        //pass in imported sequelize connection (direct connection to our database)
-        sequelize,
-        //don't automatically create createdAt/updatedAt timestamp fields
-        timestamps: false,
-        //don't pluralize name of database table
-        freezeTableName: true,
-        //use underscores instead of camel casing  
-        underscored: true,
-        //make it so our model name stays lowercase in the database
-        modelName: 'user'
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
     }
+  },
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
 );
-
-bcrypt.hash(myPlaintextPassword, saltRounds).then(function(hash) {
-    //store hash in your password DB
-});
-
 
 module.exports = User;
